247c70a89d141caa9c697d9c183f2703fa1f8a81
Elvis Hernandez <elvishernandezdev@gmail.com>
2024-11-01 12:46:23 -0400

    Fetch all Grammers from payload and render in a card with a two column layout

---
+++ src/app/(frontend)/talent/Card.tsx
@@ -0,0 +1,47 @@
+'use client';
+import Link from 'next/link';
+import { useState } from 'react';
+
+type GrammerCardProps = {
+  title: string;
+  author: string;
+  bio: string;
+  authorImage: string;
+  portfolioSite?: string;
+}
+
+export function GrammerCard(props: GrammerCardProps) {
+  const [showBio, setShowBio] = useState(false);
+
+  return (
+    <div
+      onMouseEnter={() => setShowBio(true)}
+      onMouseLeave={() => setShowBio(false)}
+      style={{ backgroundImage: `url(${props.authorImage})` }}
+      className={`
+        flex justify-center items-end border
+        border-white rounded w-[256px] h-[256px] sm:w-[512px]
+        sm:h-[512px] bg-cover bg-no-repeat bg-center
+      `}
+    >
+      <div
+        className="bg-white text-center text-black max-h-[100%] w-[80%] rounded mb-[0.5rem] py-[0.5rem]"
+      >
+        <h3 className="font-semibold">{props.author}</h3>
+        <h4>{props.title}</h4>
+        {!!props.portfolioSite && (
+          <Link className="text-sm text-blue-500" href={props.portfolioSite}>View Portfolio</Link>
+        )}
+        <div
+          style={{
+            maxHeight: showBio ? '128px' : '0',
+            overflowY: 'auto',
+            transition: 'max-height 0.3s linear',
+          }}
+        >
+          <p>{props.bio}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/app/(frontend)/talent/page.tsx
@@ -0,0 +1,34 @@
+import { getPayloadHMR } from '@payloadcms/next/utilities'
+import configPromise from '@payload-config'
+import { GrammerCard } from './Card'
+import { Media } from '@/payload-types'
+
+export default async function TalentPage() {
+  const payload = await getPayloadHMR({ config: configPromise })
+
+  const grammers = await payload.find({
+    collection: 'grammers',
+  })
+
+  return (
+    <div>
+      <h1>Talent</h1>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 place-items-center p-8 mx-auto gap-[2rem]">
+        {grammers.docs.map((grammer, index) => (
+          <div className={`flex w-full justify-center ${index % 2 === 0 ? 'md:justify-end' : 'md:justify-start'}`}>
+            <GrammerCard
+              key={grammer.id}
+              title={grammer.title}
+              author={grammer.name}
+              bio={grammer.bio}
+              authorImage={(grammer.author_image as Media)?.url || ""}
+              portfolioSite={grammer.portfolio_site || ""}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
