e70c90c3b7dfc3d4f7685e6f9e1aaa36aca9012c
Elvis Hernandez <elvishernandezdev@gmail.com>
2024-12-04 20:13:12 -0500

    Clear up some of the image related warning messages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
 	return (
 		<main className='items-center w-full h-full'>
 			<div className='w-full' style={{ height: '700px', position: 'relative', top: 0 }}>
-				<Image src='/images/grammerhub-bk.jpg' alt='logo' layout='fill' objectFit='cover' />
+				<Image src='/images/grammerhub-bk.jpg' alt='logo' fill />
 			</div>
 			<div className='bg-white w-full'>
 				<div className='px-6 py-24 sm:px-6 sm:py-32 lg:px-8'>
@@ -31,10 +31,11 @@ export default function Home() {
 				</div>
 			</div>
 			<div className='relative isolate overflow-hidden bg-gray-900 px-6 py-24 sm:py-32 lg:px-8'>
-				<img
+				<Image
 					alt=''
 					src='https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply'
-					className='absolute inset-0 -z-10 h-full w-full object-cover'
+					className='absolute inset-0 -z-10'
+					fill
 				/>
 				<div
 					aria-hidden='true'
