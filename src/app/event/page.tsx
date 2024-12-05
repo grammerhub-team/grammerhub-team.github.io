3d072f98de3af6192d5a61e118533e547f33aafc
Bibi <dbibien@gmail.com>
2024-12-05 01:16:34 -0500

    add event page

---
+++ src/app/event/page.tsx
@@ -0,0 +1,87 @@
+import EventCardList from "../_components/EventCardList"
+import { Hero } from "../_components/Hero"
+
+const events = [
+	{
+		title: "Event 1",
+		date: new Date(),
+		image: "/images/about-bk.jpeg",
+		description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+		sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+		Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+		Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+		Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		`,
+	},
+	{
+		title: "Event 2",
+		date: new Date(),
+		image: "/images/about-bk.jpeg",
+		description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+		sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+		Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+		Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+		Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		`,
+	},
+	{
+		title: "Event 3",
+		date: new Date(),
+		image: "/images/about-bk.jpeg",
+		description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+		sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+		Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+		Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+		Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		`,
+	},
+	{
+		title: "Event 4",
+		date: new Date(),
+		image: "/images/about-bk.jpeg",
+		description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+		sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+		Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+		Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+		Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		`,
+	},
+	{
+		title: "Event 5",
+		date: new Date(),
+		image: "/images/about-bk.jpeg",
+		description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+		sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+		Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+		Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+		Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		`,
+	},
+	{
+		title: "Event 6",
+		date: new Date(),
+		image: "/images/about-bk.jpeg",
+		description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+		sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+		Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+		Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+		Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		`,
+	},
+]
+
+export default function Event() {
+	return (
+		<>
+			<Hero
+				heading="Events"
+				subheading="Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+				bkUrl="/images/grammerhub-hackathon-2022.jpg"
+			/>
+
+			<div className='container mx-auto max-w-7xl  px-4 py-12'>
+				<EventCardList events={events} />
+			</div>
+		</>
+	)
+}
