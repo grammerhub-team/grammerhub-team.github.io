dc8e2e5de8097116d435ba4c1416005cab3dc8a9
Bibi <dbibien@gmail.com>
2024-12-08 19:47:51 -0500

    Modified the date of the 'In-Person Meetup' to include the year in order to avoid confusion. Add data for the first Grammerhub online meeting

--- src/app/event/page.tsx
+++ src/app/event/page.tsx
@@ -6,10 +6,21 @@ const events = [
 	{
 		title: "Grammerhub | In-Person Meetup",
 		location: "Guild Hall, 1447 lyons rd, Coconut Creek, Florida 33063",
-		date: "Sat Dec 21st • 12:30PM-02:30PM",
+		date: "Sat Dec 21st, 2024 • 12:30PM-02:30PM",
 		image: "/images/event/Red_Black_Christmas_Party_Flyer.png",
 		description: `Meet in person with us in Broward, Florida. GuildHall is an anime lounge with 
 		food, drink, board games, international snacks, video games, tournaments and more.
+		`,
+	},
+	{
+		title: "Grammerhub | Week 3 - Mockup Interviews",
+		location: "Online - Grammerhub Discord Channel",
+		date: "Mon Jan 20th, 2025 • 06:30PM",
+		image: "/images/event/Red_Black_Christmas_Party_Flyer.png",
+		description: `By the Third Monday of the Month, you've placed great emphasis on your educational gaps, 
+			making improvements to your digital assets, and now feel ready to sit with Senior-level Grammers who are
+			going to work with you on your interviewing skills.
+			Leetcode, white-boarding, and interview camera-facing will be your moments to shine - You got this
 		`,
 	},
 ]
