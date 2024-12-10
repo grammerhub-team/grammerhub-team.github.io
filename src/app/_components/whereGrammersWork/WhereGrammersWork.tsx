b39e21145ed3d54537bac455de2a2e6e58a18635
Bibi <dbibien@gmail.com>
2024-12-09 23:42:52 -0500

    data is now in the shape of a list of objects with keys '_id' and 'location'

--- src/app/_components/whereGrammersWork/WhereGrammersWork.tsx
+++ src/app/_components/whereGrammersWork/WhereGrammersWork.tsx
@@ -1,33 +1,63 @@
 import Marquee from "react-fast-marquee"
 
 const data = [
-	"Amex",
-	"Nvidia",
-	"City of Atlanta",
-	"City Furniture",
-	"Amazon",
-	"Vertafore",
-	"Cypher Learning",
-	"USPS",
-	"FPL",
-	"Manit",
+	{
+		_id: 1,
+		location: "Amex"
+	},
+	{
+		_id: 2,
+		location: "Nvidia"
+	},
+	{
+		_id: 3,
+		location: "City of Atlanta"
+	},
+	{
+		_id: 4,
+		location: "City Furniture"
+	},
+	{
+		_id: 5,
+		location: "Amazon"
+	},
+	{
+		_id: 6,
+		location: "Vertafore"
+	},
+	{
+		_id: 7,
+		location: "Cypher Learning"
+	},
+	{
+		_id: 8,
+		location: "USPS"
+	},
+	{
+		_id: 9,
+		location: "FPL"
+	},
+	{
+		_id: 10,
+		location: "Manit"
+	},
 ]
 
 const WhereGrammersWork = () => {
 	return (
-		<div className="bg-white pt-20">
+		<div className="bg-white pt-32">
 			<div className='mx-auto max-w-7xl px-6 lg:px-8'>
 				<h2 className='text-pretty text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl'>Where Grammers work</h2>
 
 				<Marquee
-					className="mt-24"
+					className="mt-20"
 				>
-					{data.map((location, index) =>
+					{data.map((work) =>
 						<p
-							key={index}
+							key={work?._id}
 							className="text-pretty text-3xl tracking-tight text-gray-900 mr-20"
 						>
-							{location}
+							{work?.location}
 						</p>
 					)}
 				</Marquee>
